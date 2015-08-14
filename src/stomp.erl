63e196c41d062e8c267856d9a912a6df0d630868
Jon Israelson <jisraelson@ngameinc.com>
2015-08-14 18:02:33 -0500

    Replace stomp.erl with the latest version from Ian Brown

--- src/stomp.erl
+++ src/stomp.erl
@@ -1,16 +1,14 @@
-%% --------------------------
-%% @author Ian Brown
-%% @copyright 2009 Ian Brown, 2010 Bob.sh
-%% @version 0.1
-%% @doc Module for client support of the STOMP messaging protocol 
-%% ([http://stomp.codehaus.org/Protocol]). 
-%%
-%% Originally authored by Ian Brown. Modified by Bob.sh.
-%%
-%% @end
-%% --------------------------
+%% Module for client support of the STOMP messaging protocol (http://stomp.codehaus.org/Protocol). 
+%% Version 0.1
+%% Authored by Ian Brown (spam@hccp.org)
+%% Documentation can be found at http://www.hccp.org/erlang-stomp-client.html
+%% This was an experiment to get a feel for the Erlang language, and provide simple client access to STOMP suppurting message brokers.  
+%% Please feel free to use and re-distribute as you see fit. Comments, improvements and questions welcome. 
+
 -module (stomp).
 -export ([connect/4]). %% "You sunk my scrabbleship!"
+-export ([connect/5]).
+-export ([connect/6]).
 -export ([disconnect/1]).
 -export ([subscribe/2]).
 -export ([subscribe/3]).
@@ -24,15 +22,22 @@
 -export ([commit_transaction/2]).
 -export ([abort_transaction/2]).
 -export ([on_message/2]).
+-export ([on_message_with_conn/2]).
 
-%% @doc Connect to a STOMP server and return a connection socket
-%%
 %% Example:	Conn = stomp:connect("localhost", 61613, "", "").
-%%
-%% @end
+
+
 connect (Host, PortNo, Login, Passcode)  ->
-	Message=lists:append(["CONNECT", "\nlogin: ", Login, "\npasscode: ", Passcode, "\n\n", [0]]),
+    connect(Host, PortNo, Login, Passcode, [], 1024).
+
+connect (Host, PortNo, Login, Passcode, Options)  ->
+    connect(Host, PortNo, Login, Passcode, Options, 1024).
+
+connect (Host, PortNo, Login, Passcode, Options, RecBuf)  ->
+
+	Message=lists:append(["CONNECT", "\nlogin: ", Login, "\npasscode: ", Passcode, concatenate_options(Options), "\n\n", [0]]),
 	{ok,Sock}=gen_tcp:connect(Host,PortNo,[{active, false}]),
+        inet:setopts(Sock, [{recbuf,RecBuf}]),
 	gen_tcp:send(Sock,Message),
 	{ok, Response}=gen_tcp:recv(Sock, 0),
 	[{type, Type}, _, _, _]=get_message(Response), %%UGLY!
@@ -42,52 +47,40 @@ connect (Host, PortNo, Login, Passcode)  ->
 	end,
 	Sock.
 
-%% @doc Subscribe to a named queue
-%%
+
 %% Example: stomp:subscribe("/queue/foobar", Conn).
-%%
-%% @end
+
 subscribe (Destination, Connection) ->
 	subscribe (Destination, Connection, [{"ack","auto"}]),
 	ok.
 
-%% @doc Subscribe to a named queue - with options.
-%%
-%% Example: stomp:subscribe("/queue/foobar", Conn, [{"ack", "client"}]).
-%% Example: stomp:subscribe("/queue/foobar", Conn, [{"ack", "client"}, {"activemq.prefetchSize", 1}]).
-%%
-%% @end
+%%  Example: stomp:subscribe("/queue/foobar", Conn, [{"ack", "client"}]).
+%%  Example: stomp:subscribe("/queue/foobar", Conn, [{"ack", "client"}, {"activemq.prefetchSize", 1}]).
+
+
 subscribe (Destination, Connection, Options) ->
-	Message=lists:append(["SUBSCRIBE", "\ndestination: ", Destination, concatenate_options(Options),"\n\n", [0]]),
+	Message=lists:append(["SUBSCRIBE", "\ndestination: ", Destination, concatenate_options(Options), "\n\n", [0]]),
 	gen_tcp:send(Connection,Message),
 	ok.
 	
-%% @doc Remove an existing subscription
-%%
+
 %% Example: stomp:unsubscribe("/queue/foobar", Conn).
-%%
-%% @end
+	
 unsubscribe (Destination, Connection) ->
 	Message=lists:append(["UNSUBSCRIBE", "\ndestination: ", Destination, "\n\n", [0]]),
 	gen_tcp:send(Connection,Message),
 	ok.	
-
-%% @doc Disconnect gracefully from the existing connection
-%%
+	
 %% Example: stomp:disconnect(Conn).
-%%
-%% @end
+	
 disconnect (Connection) ->
 	Message=lists:append(["DISCONNECT", "\n\n", [0]]),
 	gen_tcp:send(Connection,Message),
 	gen_tcp:close(Connection),
 	ok.	
 
-%% @doc Get a particular message ID
-%%
 %% Example: stomp:get_message_id(Message).
-%%
-%% @end
+
 get_message_id ([_, {headers, Headers}, _]) ->
 	get_message_id (Headers);
 get_message_id ([H|T]) ->
@@ -98,13 +91,14 @@ get_message_id ([H|T]) ->
 get_message_id ([])	->
 	throw("No header with name of 'message-id' was found.").
 	
-%% @doc Acknowledge consumption of a message
-%%
+	
+	
+	
+	
 %% Example: stomp:ack(Conn, Message).
 %% Example: stomp:ack(Conn, stomp:get_message_id(Message)).
 %% Example: stomp:ack(Conn, "ID:phosphorus-63844-1247442885553-3:1:1:1:1").
-%%
-%% @end
+
 ack (Connection, [Type, Headers, Body]) ->
 	MessageId=get_message_id([Type, Headers, Body]),
 	ack(Connection, MessageId);
@@ -113,13 +107,12 @@ ack (Connection, MessageId)	->
 	gen_tcp:send(Connection,AckMessage),
 	ok.
 
-%% @doc Acknowledge consumption of a message
-%%
+
+
 %% Example: stomp:ack(Conn, Message, TransactionId).
 %% Example: stomp:ack(Conn, stomp:get_message_id(Message), TransactionId).
 %% Example: stomp:ack(Conn, "ID:phosphorus-63844-1247442885553-3:1:1:1:1", TransactionId).
-%%
-%% @end
+
 ack (Connection, [Type, Headers, Body], TransactionId) ->
 	MessageId=get_message_id([Type, Headers, Body]),
 	ack(Connection, MessageId, TransactionId);
@@ -128,71 +121,90 @@ ack (Connection, MessageId, TransactionId)	->
 	gen_tcp:send(Connection,AckMessage),
 	ok.
 
-%% @doc Send a message to the destination in the messaging system
-%%
 %% Example: stomp:send(Conn, "/queue/foobar", [], "hello world").
 %% Example: stomp:send(Conn, "/queue/foobar", [{"priority","15"}], "high priority hello world").
-%%
-%% @end
+	
 send (Connection, Destination, Headers, MessageBody) ->
 	Message=lists:append(["SEND", "\ndestination: ", Destination, concatenate_options(Headers), "\n\n", MessageBody, [0]]),
 	gen_tcp:send(Connection,Message),
 	ok.
 		
-%% @doc Retrieve messages destined for this client from the server.
-%%
+		
+	
+	
+
 %% Example: stomp:get_messages(Conn).
-%%
-%% @end
+
 get_messages (Connection) ->
 	get_messages (Connection, []).
 	
 get_messages (Connection, Messages) ->
-	{ok, Response}=gen_tcp:recv(Connection, 0),
+	Response=do_recv(Connection),
 	get_messages(Connection, Messages, Response).
 		
 get_messages (_, Messages, []) ->
 	Messages;
 get_messages (Connection, Messages, Response) ->
 			[{type, Type}, {headers, Headers}, {body, MessageBody}, TheRest]=get_message(Response),
-			[_|T]=TheRest, %% U.G.L.Y. . . .  you ain't got no alibi.
-			get_messages (Connection, lists:append(Messages, [[{type, Type}, {headers, Headers}, {body, MessageBody}]]), T).
+			
+			get_messages (Connection, lists:append(Messages, [[{type, Type}, {headers, Headers}, {body, MessageBody}]]), get_rest(TheRest)).
 
-%% @doc On message retrieval execute a function
-%%
+%% U.G.L.Y. . . .  you ain't got no alibi.
+%% 6/24/11 I think the rest is when more than one message is retrived at at given time...in any case, looks like large messages are sometimes missing an expected terminationg 0 char?
+%% 6/24/11 ahh...the actual issue is when the message exceeds the read window size, we don't have the entire message...so it looks like it is not terminated beacuse it is not yet terminated
+get_rest(TheRest)->
+    case TheRest of
+	[]->[];
+        [_|T]->T
+    end.
+	       
+
+do_recv(Connection)->
+    do_recv(Connection,[]).
+
+do_recv(Connection, [])->
+    {ok, Response}=gen_tcp:recv(Connection, 0),
+    do_recv(Connection, Response);
+do_recv(Connection, Response)->
+    {Status, Data}=gen_tcp:recv(Connection, 0, 1000),
+    case Status of
+	ok->
+	    do_recv(Connection, lists:flatten([Response, Data]));
+	error -> Response
+    end.
+
 %% Example: MyFunction=fun([_, _, {_, X}]) -> io:fwrite("message ~s ~n", [X]) end, stomp:on_message(MyFunction, Conn).
-%%
-%% @end
+
 on_message (F, Conn) ->
 	Messages=get_messages(Conn),
 	apply_function_to_messages(F, Messages),
 	on_message(F, Conn).
 
-%% @doc Begin a transaction
-%%
+
+on_message_with_conn (F, Conn) ->
+	Messages=get_messages(Conn),
+	apply_function_to_messages(F, Messages, Conn),
+	on_message_with_conn(F, Conn).
+
+
 %% Example: stomp:begin_transaction(Conn, "MyUniqueTransactionIdBlahBlahBlah1234567890").
-%%
-%% @end
+
 begin_transaction (Connection, TransactionId) ->
 	Message=lists:append(["BEGIN", "\ntransaction: ", TransactionId, "\n\n", [0]]),
 	gen_tcp:send(Connection,Message),
 	ok.
 
-%% @doc Commit a transaction
-%%
+
 %% Example: stomp:commit_transaction(Conn, "MyUniqueTransactionIdBlahBlahBlah1234567890").
-%%
-%% @end
+	
 commit_transaction (Connection, TransactionId) ->
 	Message=lists:append(["COMMIT", "\ntransaction: ", TransactionId, "\n\n", [0]]),
 	gen_tcp:send(Connection,Message),
 	ok.
 		
-%% @doc Abort a transaction
-%%
+
 %% Example: stomp:abort_transaction(Conn, "MyUniqueTransactionIdBlahBlahBlah1234567890").
-%%
-%% @end
+
 abort_transaction (Connection, TransactionId) ->
 	Message=lists:append(["ABORT", "\ntransaction: ", TransactionId, "\n\n", [0]]),
 	gen_tcp:send(Connection,Message),
@@ -211,10 +223,18 @@ apply_function_to_messages(F, [H|T]) ->
 	F(H),
 	apply_function_to_messages(F, T).
 
+apply_function_to_messages(_, [],_) ->
+	ok;
+apply_function_to_messages(F, [H|T], Conn) ->
+	F(H, Conn),
+	apply_function_to_messages(F, T, Conn).
+
 % MESSAGE PARSING  . . . get's a little ugly in here . . . would help if I truly grokked Erlang, I suspect.
 % 7/12/09 - yeah, ugly indeed, i need to make this use the same pattern as get_headers_from_raw_src . . . currently scanning header block multiple times and making unnecessary copies
 get_message(Message) ->
+   
  	[Type, {Headers, MessageBody}, TheRest]=get_type(Message), %% Ugly . . .
+
 	{ParsedHeaders, _}=get_headers_from_raw_src([], Headers),
 	[{type, Type}, {headers, ParsedHeaders}, {body, MessageBody}, TheRest].
 
@@ -224,13 +244,22 @@ get_message(Message) ->
 	
 %% extract message body
 get_message_body ([H|T]) ->
+                %% io:write([H|T]),
+                %% io:fwrite("~n",[]),
+                %% io:write(H),
 		get_message_body ([H|T], []).
 
 get_message_body ([H|T], MessageBody) ->
+             %%io:fwrite("~n",[]),
+             %%io:fwrite("In get msg body/2 ~n",[]),
+             %%io:write([H|T]),
 	case(H) of
 		0 -> {MessageBody, T};
 		_ -> {MyMessageBody, TheRest}=get_message_body(T, MessageBody), {lists:append([MessageBody, [H], MyMessageBody]), TheRest}
-	end.	
+	end;	
+get_message_body ([],[]) ->
+    {[],[]}.
+
 
 
 %% extract headers as a blob of chars, after having iterated over . . .
@@ -286,6 +315,5 @@ get_header_value (HeaderValue, [H|T]) ->
 			10 -> {HeaderValue, T};
 			_ -> get_header_value(lists:append([HeaderValue, [H]]), T)
 			end.	
-
 
 
